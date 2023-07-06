8ca359714f676b8a71de4642c11f5368fd7ca150
Carmen <mcchristy10@yahoo.com>
2023-07-06 15:57:08 -0400

    created an array of questions for user input to create the readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
