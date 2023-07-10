c917e7fd68278a3efcd72df2de5f945688412939
Carmen <mcchristy10@yahoo.com>
2023-07-10 17:05:18 -0400

    coded a function to write the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -52,7 +52,15 @@ const questions = [
   ];
 
 // TODO: Create a function to write README file
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`${fileName} created successfully!`);
+      }
+    });
+  }
 
 // TODO: Create a function to initialize app
 
